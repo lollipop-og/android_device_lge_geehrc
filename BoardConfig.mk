06f59aebb7c9aa6b406c05178cdd222db70022e8
Kevindeving <98kevchen@gmail.com>
2015-07-12 16:53:13 +0200

    geehrc: readd mrom support

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -81,6 +81,7 @@ ifeq ($(HOST_OS),linux)
 endif
 WITH_DEXPREOPT_BOOT_IMG_ONLY ?= true
 
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 TARGET_RECOVERY_FSTAB = device/lge/geehrc/fstab.geehrc
 RECOVERY_FSTAB_VERSION = 2
 TARGET_USERIMAGES_USE_EXT4 := true
@@ -171,6 +172,15 @@ TW_EXTERNAL_STORAGE_PATH := "/usb-otg"
 TW_EXTERNAL_STORAGE_MOUNT_POINT := "usb-otg"
 TW_BRIGHTNESS_PATH := /sys/class/backlight/lm3530/brightness
 TW_MAX_BRIGHTNESS := 255
+
+#MultiROM config. MultiROM also uses parts of TWRP config
+MR_INPUT_TYPE := type_b
+MR_INIT_DEVICES := device/lge/geehrc/mr_init_devices.c
+MR_RD_ADDR := 0x82000000
+MR_DPI := hdpi
+MR_DPI_FONT := 216
+MR_FSTAB := device/lge/geehrc/recovery/twrp.fstab
+MR_KEXEC_MEM_MIN := 0x84000000
 
 #Asserts
 TARGET_OTA_ASSERT_DEVICE := gee,geeb,geehrc,e975,geehrc_4g_spr,geespr,ls970,geehrc4g,geehrc4g_spr_us,geebus,e970,e973,e971,geeb_att_us
