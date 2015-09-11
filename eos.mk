585dcde97479c3343cb1df1a0ca4b288592217f9
Kevindeving <98kevchen@gmail.com>
2015-09-11 10:27:02 -0400

    geehrc: init eos bringup

--- eos.mk
+++ eos.mk
@@ -2,21 +2,20 @@
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 768
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common EOS stuff.
+$(call inherit-product, vendor/eos/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/geehrc/full_geehrc.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := geehrc
-PRODUCT_NAME := cm_geehrc
+PRODUCT_NAME := eos_geehrc
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := Optimus G
 PRODUCT_MANUFACTURER := LGE
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-	USER=Dadi11 \
 	PRODUCT_NAME=geehrc_open_eu \
 	BUILD_FINGERPRINT=lge/geehrc_open_eu/geehrc:4.4.2/KOT49I.E97520a/E97520a.1403273885:user/release-keys \
 	PRIVATE_BUILD_DESC="geehrc_open_eu-user 4.4.2 KOT49I.E97520a E97520a.1403273885 release-keys"
